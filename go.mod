836a2d779097207ef4b0b0774840facf50103306
Francisco Rodera <trek1s@gmail.com>
2023-05-20 15:20:41 +1000

    1st commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/frodera/stringutils
+
+go 1.20
---
+++ stringutils.go
@@ -0,0 +1,12 @@
+package stringutils
+
+import "strings"
+
+// function to convert string to uppercase
+func Upper(s string) string {
+	return strings.ToUpper(s)
+}
+
+func Lower(s string) string {
+	return strings.ToLower(s)
+}
